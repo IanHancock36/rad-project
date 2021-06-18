3c3b3cdb92f13ec8ec85407e0de2814c7b5c9f5b
Ian Hancock <northroadian@gmail.com>
2021-06-18 07:00:14 -0500

    adding to this project

---
+++ Components/ImageData.js
@@ -0,0 +1,13 @@
+export default ImageData  = [
+    {
+    title: 'Thing 1',
+    body: 'This is a thing ',
+    imageUrl: 'https://www.google.com/search?q=thing+1&client=safari&rls=en&tbm=isch&source=iu&ictx=1&fir=BF07vMeTPqfH1M%252CJJWKwe9F0C6QPM%252C_&vet=1&usg=AI4_-kQYqjNcXIDQPz8ZXFCLll9xcnN3UQ&sa=X&ved=2ahUKEwja14yikKHxAhVVu54KHdFVC0AQ_h16BAgJEAE#imgrc=BF07vMeTPqfH1M'
+    },
+
+    { title: 'Thing 2',
+      body: 'this is Thing 2',
+      imageUrl: 'https://www.google.com/search?q=thing+2&client=safari&rls=en&tbm=isch&source=iu&ictx=1&fir=Tsl7y-luJer4hM%252C4Zh7CNeHUH0caM%252C_&vet=1&usg=AI4_-kRd1EmNfz6XYzrx1v2P6VBvW0EHhQ&sa=X&ved=2ahUKEwirqJ7FkKHxAhVI-J4KHT_WA9QQ_h16BAgJEAE#imgrc=Tsl7y-luJer4hM'
+ 
+    }
+]
---
+++ Components/ImageSlider.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {View,Text, Stylesheet, Dimensions, Image} from 'react-native'
+
+import ImageDate from './Components/ImageData' 
+
+
+const ImageSlider = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ImageSlider
+
+
+//need to import images from another list or a list. 
+    // needs to be objects inside an array
+        //cycle through the array or map and + 1 to the image so it cycles to next image
+            //use state 
+                // touchable opacity for the swipe? do I need an Onpress event added to it to update the state?  
+// use a flatlist for part of comonent.
+// need to update state 
